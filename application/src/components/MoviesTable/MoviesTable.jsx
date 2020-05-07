e077a31a60f9b2490ab80530b6b5fd683d442633
viktor <vkibank@gmail.com>
2020-05-07 21:48:52 +0300

    Display real data from server

--- application/src/components/MoviesTable/MoviesTable.jsx
+++ application/src/components/MoviesTable/MoviesTable.jsx
@@ -53,9 +53,9 @@ class MoviesTable extends React.Component {
   render() {
     const { anchorEl, openDialog, data: activeElem = {} } = this.state;
 
-    const { classes } = this.props;
+    const { classes, data = {} } = this.props;
 
-    console.log(this.props.data);
+    const { movies = [] } = data;
 
     return (
       <>
@@ -79,7 +79,7 @@ class MoviesTable extends React.Component {
                     <TableCell component="th" scope="row">{movie.name}</TableCell>
                     <TableCell>{movie.genre}</TableCell>
                     <TableCell align="right">{movie.rate}</TableCell>
-                    <TableCell>{movie.director.name}</TableCell>
+                    <TableCell>{movie.director ? movie.director.name : ''}</TableCell>
                     <TableCell>
                       <Checkbox checked={movie.watched} disabled />
                     </TableCell>
